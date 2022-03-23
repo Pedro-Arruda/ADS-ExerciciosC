3cce14d91bd646f8d46b7b79b395b0c5cf1dcfb5
Pedro Arruda <arruda.pedro15@outlook.com>
2022-03-23 08:33:26 -0300

    commit

--- Lista2-ex07.cpp
+++ Lista2-ex07.cpp
@@ -1,34 +1,43 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
 int main()
 {
-    float diarias, consumo, total_diaria, subtotal, taxa, total_geral;
-    char nome [20], apartamento[10];
+    float diarias, consumo, total_diaria, subtotal, taxa, total_geral, valor_diaria;
+    char nome [20], apartamento[1];
+
     cout << "Digite seu nome: " << endl;
     cin >> nome;
+
     cout << "Digite o tipo de apartamento: " << endl;
     cin >> apartamento;
+
     cout << "Digite o numero de diarias: " << endl;
     cin >> diarias;
+
     cout << "Digite o valor do consumo interno: " << endl;
     cin >> consumo;
 
-    if (apartamento == "a" || apartamento == "A"){
-        total_diaria = diarias * 150;
+    if (strcmp(apartamento, "a" ) == 0){
+        valor_diaria = 150;
+        total_diaria = (diarias * valor_diaria);
     }
 
-    if (apartamento == "b" || apartamento == "B"){
-        total_diaria = diarias * 100;
+    if (strcmp(apartamento, "b") == 0) {
+        valor_diaria = 100;
+        total_diaria = diarias * valor_diaria;
     }
 
-    if (apartamento == "c" || apartamento == "C"){
-        total_diaria = diarias * 75;
+    if (strcmp(apartamento, "c") == 0){
+        valor_diaria = 75;
+        total_diaria = diarias * valor_diaria;
     }
 
-    if (apartamento == "d" || apartamento == "D"){
-        total_diaria = diarias * 50;
+    if (strcmp(apartamento, "d") == 0){
+        valor_diaria = 50;
+        total_diaria = diarias * valor_diaria;
     }
 
     subtotal = total_diaria + consumo;
@@ -37,6 +46,14 @@ int main()
 
     total_geral = subtotal + taxa;
 
-    cout << diarias << "/t" << total_diaria;
+    cout << "Nome: " << nome <<endl;
+    cout << "Apartamento: " << apartamento <<endl;
+    cout << "Numero de diarias: " << diarias <<endl;
+    cout << "Valor da Diaria: " << valor_diaria <<endl;
+    cout << "Valor total das diarias: " << total_diaria <<endl;
+    cout << "Valor do consumo interno: " << consumo <<endl;
+    cout << "Subtotal: " << subtotal <<endl;
+    cout << "Taxa: " << taxa <<endl;
+    cout << "Total geral: " << total_geral <<endl;
 
 }
